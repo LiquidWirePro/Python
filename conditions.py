8603286b95b794d0dc6daa2ee00134f4b4c5244a
LiquidWirePro <liquidwirepro@gmail.com>
2020-07-14 22:33:08 +0200

    final commit

---
+++ conditions.py
@@ -0,0 +1,23 @@
+
+# if
+#a = 5
+#b = 8
+#if a > 0:
+#    b += 1
+#    print("a =", a,"et b =",b)
+
+# else
+#ge = 22
+#if age >= 18:
+#    print("Vous etes majeur.")
+#else:
+#    print("Vous n'etes pas majeur.")
+
+# elif
+a = 8
+if a > 0:
+    print("a est superieur a 0.")
+elif a < 0:
+    print("a est inferieur a 0.")
+else:
+    print("a est nul.")
